d3475aeb0a7a1a05a9181f12a6998f64f5eb8628
Victor <kidaisuda@yahoo.com>
2015-11-18 20:52:35 -0800

    Test

--- application.py
+++ application.py
@@ -7,24 +7,27 @@ from sqlalchemy import create_engine
 application = Flask(__name__)
 api = Api(application)
 application.debug = True
+
+
 DB_STRING = 'mysql+pymysql://%s:%s@%s:%s/%s' % ('user', 'password', 'projectdb.ckapg67klplg.us-west-1.rds.amazonaws.com', 3306, 'db1')
 engine = create_engine(DB_STRING, pool_recycle=3600)
 query = 'SELECT * FROM users'
 conn = engine.connect()
 r = conn.execute(query).cursor.fetchall()
-print(r)
 
 
 @application.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+@application.route('/api', methods=['GET'])
+def index1():
+    return render_template('index1.html')
+
 
 class User(Resource):
     def get(self):
-        resp = Response(json.dumps({'test':'test'}))
-        resp.status_code = 200
-        return resp
+        return {'test':'test'}, 200
 
 api.add_resource(User, '/api/v1/users')
 
