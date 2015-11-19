291cff322cc70a72476f1a5e57302772bddaf40c
Ilya Stepanko <ilya@aylanetworks.com>
2015-11-18 21:06:24 -0800

    34

--- application.py
+++ application.py
@@ -11,22 +11,19 @@ application.debug = True
 
 DB_STRING = 'mysql+pymysql://%s:%s@%s:%s/%s' % ('user', 'password', 'projectdb.cv6b9gyk6jxg.us-west-1.rds.amazonaws.com', 3306, 'db1')
 engine = create_engine(DB_STRING, pool_recycle=3600)
-query = 'SELECT * FROM users'
-conn = engine.connect()
-r = conn.execute(query).cursor.fetchall()
 
 
 @application.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@application.route('/api', methods=['GET'])
-def index1():
-    return render_template('index1.html')
-
 
 class User(Resource):
     def get(self):
+        query = 'SELECT * FROM users'
+        conn = engine.connect()
+        r = conn.execute(query).cursor.fetchall()
+
         return {'test':'test'}, 200
 
 api.add_resource(User, '/api/v1/users')
